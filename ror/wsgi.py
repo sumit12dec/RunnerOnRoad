921ed2cec91f5c4addeef4d916bc02f779ae60e1
Sumit Kumar Raj <sumit.raj@commonfloor.com>
2015-11-29 13:20:22 +0530

    prod and dev settings separated

--- ror/wsgi.py
+++ ror/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
 #from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/var/www/RunnerOnRoad')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ror.settings")
 
 # application = Cling(get_wsgi_application())
